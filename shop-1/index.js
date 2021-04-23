5c5722e893c6c89b74df6706f9988af06b75f842
Jan Voracek <jan@voracek.net>
2021-04-23 08:05:51 +0000

    Change shop

--- shop-1/index.js
+++ shop-1/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-    res.write('Hello Banana 16');
+    res.write('Hello Banana 17');
     res.end();
 }).listen(parseInt(process.env.PORT, 10));
