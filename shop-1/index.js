4f27bffd4bd93d58f7358696845006b1c2e212a3
Jan Voracek <jan@voracek.net>
2021-07-20 11:48:04 +0200

    Update index.js

--- shop-1/index.js
+++ shop-1/index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-    res.write('Hello Banana 30');
+    res.write('Hello Banana 31');
     res.end();
 }).listen(parseInt(process.env.PORT, 10));
