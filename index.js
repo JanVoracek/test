6bf105ffbf8b6e1f7447087f256e8a35bf23b2cc
Jan Voracek <jan@voracek.net>
2021-10-13 12:11:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const banana = require("./@banana/banana");
 
 http
   .createServer(function (req, res) {
-    res.write(`Hello ${banana} 45`);
+    res.write(`Hello ${banana} 46`);
     res.end();
   })
   .listen(parseInt(process.env.PORT, 10));
