d8ccb51aea98d807278186dc75227d09b88a6ed7
Jan Voracek <jan@voracek.net>
2021-11-23 07:43:54 +0100

    Change fenix

--- shops/sofa-fenix/index.js
+++ shops/sofa-fenix/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    res.write(`Fenix 3`);
+    res.write(`Fenix 4`);
     res.end();
   })
   .listen(parseInt(process.env.PORT, 10));
