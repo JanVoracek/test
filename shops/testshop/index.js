3b40f98e74561b7db8fd1cddef5e45686b25363c
Jan Voracek <jan@voracek.net>
2021-11-23 07:44:48 +0100

    Change both shops

--- shops/testshop/index.js
+++ shops/testshop/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    res.write(`Hello World 51`);
+    res.write(`Hello World 52`);
     res.end();
   })
   .listen(parseInt(process.env.PORT, 10));
