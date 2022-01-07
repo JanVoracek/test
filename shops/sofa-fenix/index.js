41e180c9051b4c6e4dd78586e52e8d3c7754c7d4
Jan Voracek <jan@voracek.net>
2022-01-07 10:28:49 +0100

    Update shops

--- shops/sofa-fenix/index.js
+++ shops/sofa-fenix/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 http
   .createServer(function (req, res) {
-    res.write(`Fenix 7`);
+    res.write(`Fenix 8`);
     res.end();
   })
   .listen(parseInt(process.env.PORT, 10));
